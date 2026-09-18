366d6ad19f1c7c4725b515e09a963bc3ed5d9fc7
LimeNyan25 <lhg92606273@gmail.com>
2026-09-18 12:16:59 +0900

    3주차_점프

--- ullaulla/Assets/playercontroller.cs
+++ ullaulla/Assets/playercontroller.cs
@@ -1,14 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.LowLevelPhysics2D.PhysicsLayers;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Vector2 moveInput;
+    public string PalyerName = "Player";
+    public int hp = 100;
+    public Transform visual;
+    bool isAlive = true;
+    Vector3 startPosition;
 
-    void OnMove(InputValue value)
+    public float jumpPower = 8f;
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        Debug.Log(PalyerName + " 시작. 체력 " + hp);
+        Debug.Log("피격 후 체력 " + (hp - 30));
+        Debug.Log("달리기 속도 " + moveSpeed * 2);
+        Debug.Log("10f/ 4f= " + 10f / 4f);
+
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = startPosition;
+    }
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log("점프!");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+
+
+        }
+    }
+
+
+        void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        if (moveInput.x > 0)
+        {
+            visual.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            visual.localScale = new Vector3(-1, 1, 1);
+        }
+
     }
 
     void Update()
